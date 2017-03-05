83327e75ef230e3fd098c74e7ddc9a51290f9b7d
Rajdeep <rajdeeps.iitb@gmail.com>
2017-03-05 12:21:37 +0900

    added question and source

--- FindTrippletWithZeroSum.java
+++ FindTrippletWithZeroSum.java
@@ -1,3 +1,15 @@
+/*
+http://www.geeksforgeeks.org/find-triplets-array-whose-sum-equal-zero/
+
+Given an array of distinct elements. The task is to find triplets in array whose sum is zero
+
+Input : arr[] = {0, -1, 2, -3, 1}
+Output : 0 -1 1
+         2 -3 1
+
+Input : arr[] = {1, -2, 1, 0, 5}
+Output : 1 -2  1
+*/
 import java.util.*;
 import java.io.*;
 
